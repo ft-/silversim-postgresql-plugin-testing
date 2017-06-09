eb41285739a09b775d0e2a361c38bbabe029abdd
ft <ft@noemail>
2017-06-09 17:23:41 +0200

    code cleanup

--- SilverSim/Database.PostgreSQL/SimulationData/PostgreSQLSimulationDataStorage.ScriptStateStorage.cs
+++ SilverSim/Database.PostgreSQL/SimulationData/PostgreSQLSimulationDataStorage.ScriptStateStorage.cs
@@ -53,7 +53,6 @@ namespace SilverSim.Database.PostgreSQL.SimulationData
             return false;
         }
 
-        /* setting value to null will delete the entry */
         byte[] ISimulationDataScriptStateStorageInterface.this[UUID regionID, UUID primID, UUID itemID]
         {
             get
